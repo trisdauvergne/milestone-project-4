c359e27effc818eee5bbf1e5b553331f6dd942e7
trisdauvergne <tristan.dauvergne@gmail.com>
2020-05-06 12:43:09 +0000

    200506 - 14:42 feat: added 'prints' models for designers and prints then linked to admin

---
+++ prints/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+
+from .models import Designer, Print
+
+admin.site.register(Designer)
+
+admin.site.register(Print)
---
+++ prints/models.py
@@ -0,0 +1,35 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Designer(models.Model):
+
+    full_name = models.CharField(max_length=50, null=False, blank=False)
+    country = models.CharField(max_length=50, null=False, blank=False)
+    bio = models.CharField('A few words about the designer',
+                           max_length=254, null=False, blank=False)
+    email = models.EmailField(max_length=254, null=False, blank=False)
+
+    def __str__(self):
+        return self.full_name
+
+    class Meta:
+        verbose_name = 'Designer'
+
+
+class Print(models.Model):
+
+    designer = models.ForeignKey('Designer', null=True, blank=True, on_delete=models.CASCADE)
+    title = models.CharField(max_length=254, null=False, blank=False)
+    description = models.CharField('A few words about this work',
+                                   max_length=254, null=False, blank=False)
+    size = models.CharField(max_length=30, null=False, blank=False)
+    price = models.CharField(max_length=30, null=False, blank=False)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Print'
+

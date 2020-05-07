0b6188a215beb06ac32b39ae7d12949c4b938bcf
trisdauvergne <tristan.dauvergne@gmail.com>
2020-05-07 16:15:42 +0000

    200507 - 18:15 feat: added user login page template, view and path

--- users/urls.py
+++ users/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from .import views
 
 urlpatterns = [
-    path('', views.user_registration, name='user_registration'),
+    path('registration', views.user_registration, name='user_registration'),
+    path('login', views.user_login, name='user_login'),
 ]
--- users/views.py
+++ users/views.py
@@ -5,3 +5,9 @@ def user_registration(request):
     """ A view to show the registration page """
     return render(request,
                   'users/user_registration.html')
+
+
+def user_login(request):
+    """ A view to show the login page"""
+    return render(request,
+                  'users/user_login.html')

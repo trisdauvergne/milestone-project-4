fe9a69cbcf6da514178f362ce2c10bf35de2ffef
trisdauvergne <tristan.dauvergne@gmail.com>
2020-05-08 17:38:23 +0000

    200508 - 19:37 feat: added code to all_designers.html to filter artist results

--- designers/urls.py
+++ designers/urls.py
@@ -3,5 +3,5 @@ from .import views
 
 urlpatterns = [
     path('', views.all_designers, name='all_designers'),
-    path('designer-detail/', views.designer_detail, name='designer_detail'),
+    path('designer-detail/<designer_id>/', views.designer_detail, name='designer_detail'),
 ]
--- designers/views.py
+++ designers/views.py
@@ -7,7 +7,8 @@ from .models import Designers
 
 def all_designers(request):
     """ A view to return the all designers page """
-    designers = Designers.objects.all()
+    designer = Designers.objects.all()
+    designers = designer.order_by('full_name')
     prints = Print.objects.all()
 
     context = {
@@ -20,12 +21,14 @@ def all_designers(request):
                   context)
 
 
-def designer_detail(request):
+def designer_detail(request, designer_id):
     """ A view to return detail about a specific designer """
-    designer = Designers.objects.get(pk=4)
+    designer = Designers.objects.get(id=designer_id)
+    prints = Print.objects.filter(designer=designer)
 
     context = {
         'designer': designer,
+        'prints': prints,
     }
 
     return render(request,

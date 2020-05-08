d83c61c7de7e6564cfb71861621c28764b0634dd
trisdauvergne <tristan.dauvergne@gmail.com>
2020-05-08 06:53:41 +0000

    200508 - 08:53 feat: added display prints function to prints page, tested and prints displayed

--- prints/views.py
+++ prints/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
 
+from .forms import Print
+
 
 def all_prints(request):
     """ A view to show all the prints on Planchest """
+    prints = Print.objects.all()
+
+    context = {
+        'prints': prints,
+    }
+
     return render(request,
-                  'prints/all_prints.html')
+                  'prints/all_prints.html',
+                  context)
